6ddc9227b68fe97f2c5107a5b0d06067b7b210f7
Adrian Aisemberg <adrianaisemberg@gmail.com>
2021-10-02 19:03:38 -0700

    cleaup

--- TicTacToe/app/src/main/java/com/adrianaisemberg/tictactoe/utils/ContextUtils.kt
+++ TicTacToe/app/src/main/java/com/adrianaisemberg/tictactoe/utils/ContextUtils.kt
@@ -4,5 +4,3 @@ import android.content.Context
 import android.view.LayoutInflater
 
 fun Context.layoutInflater(): LayoutInflater = LayoutInflater.from(this)
-
-fun Context.version(): String = packageManager.getPackageInfo(packageName, 0).versionName
--- TicTacToe/app/src/main/java/com/adrianaisemberg/tictactoe/utils/Delegates.kt
+++ TicTacToe/app/src/main/java/com/adrianaisemberg/tictactoe/utils/Delegates.kt
@@ -2,8 +2,3 @@ package com.adrianaisemberg.tictactoe.utils
 
 typealias Action = () -> Unit
 typealias ActionOf<T> = (t: T) -> Unit
-typealias Func<TIn, TOut> = (t: TIn) -> TOut
-typealias FuncOf<TOut> = () -> TOut
-
-val NOOP = {}
-
